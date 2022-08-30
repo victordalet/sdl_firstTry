65e31c01e877d57197032b88717c81b623187054
victordalet <victordalet@protonmail.com>
2022-08-30 14:38:55 +0200

    first window

--- main.c
+++ main.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "include/SDL.h"
 
-int main() {
-    for (int i = 0 ; i < 10 ; i++){
-        printf("Hello, World!\n");}
-    return 0;
+
+void SDL_ExitWithError(const char *message) {
+    SDL_Log("ERREUR : %s > %s\n", message, SDL_GetError());
+    SDL_Quit();
+    exit(EXIT_FAILURE);
 }
+
+int main(int argc, char** argv) {
+    SDL_Window *window = NULL;
+
+    if(SDL_Init(SDL_INIT_VIDEO) != 0)
+        SDL_ExitWithError("Initialisation SDL");
+
+    window = SDL_CreateWindow("Première fenêtre SDL 2", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
+
+    if(window == NULL)
+        SDL_ExitWithError("Creation fenetre echouee");
+
+    SDL_Delay(6000);
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return EXIT_SUCCESS;
+}
+
